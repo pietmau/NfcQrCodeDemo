29e3531fc8615af628f965a1b993dae2b4d806c3
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-02-23 11:17:47 +0000

    Handle url

--- NfcQrCodeDemo/AppDelegate.swift
+++ NfcQrCodeDemo/AppDelegate.swift
@@ -11,35 +11,41 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+
     var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
     }
-    
+
     func application(_ application: UIApplication, continue userActivity: NSUserActivity,
                      restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
         guard let dynamicLinks = DynamicLinks.dynamicLinks() else {
             return false
         }
         let handled = dynamicLinks.handleUniversalLink(userActivity.webpageURL!) { (dynamiclink, error) in
-            // ...
+            if let dynamiclink = dynamiclink, let _ = dynamiclink.url {
+                if let url = (dynamiclink as? DynamicLink)?.url {
+                    self.handleUrl(url: url)
+                }
+            }
         }
-        
         return handled
     }
-    
-    
-    @available(iOS 9.0, *)
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
-        return application(app, open: url,
-                           sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
-                           annotation: "")
+
+
+    private func handleUrl(url url: URL) {
+
     }
-    
+
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any]) -> Bool {
+        return application(app, open: url,
+                sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                annotation: "")
+    }
+
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         if let dynamicLink = DynamicLinks.dynamicLinks()?.dynamicLink(fromCustomSchemeURL: url) {
             return true
