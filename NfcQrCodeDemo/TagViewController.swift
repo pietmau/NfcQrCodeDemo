f0b3c767913cec8fabe9135013ec7b7b38fa1265
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-02-23 15:55:08 +0000

    UI for tags

---
+++ NfcQrCodeDemo/TagViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TagViewController.swift
+//  NfcQrCodeDemo
+//
+//  Created by Maurizio Pietrantuono on 23/02/2018.
+//  Copyright © 2018 Maurizio Pietrantuono. All rights reserved.
+//
+
+import UIKit
+
+class TagViewController: UIViewController {
+
+    @IBAction func onButtonClick(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
